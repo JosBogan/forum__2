3d1e2da0100ae981916f602a841b80e52e264067
Jos Bogan <Josbogan@gmail.com>
2021-01-15 14:00:01 +0000

    recommiting

--- config/environment.js
+++
@@ -1,6 +0,0 @@
-const base = process.env.NODE_ENV || 'development'
-const port = 4000
-const dbURI = `mongodb://localhost/forum-${base}` // Location of our database on our local machine
-const secret = 'secret dsad'
-
-module.exports = { port, dbURI, base } 
