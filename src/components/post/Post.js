b602146a016a74ab79e2cb2fadf31af21e11f9f7
Jos Bogan <Josbogan@gmail.com>
2021-01-30 13:51:37 +0000

    added commenting on posts

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 import '../../styles/post.css'
 
+import Auth from '../../lib/auth'
+
 const Post = (props) => {
 
   const [postData, setPostData] = useState({})
+  const [commentData, setCommentData] = useState('')
 
   useEffect(() => {
     getData()
@@ -14,12 +18,39 @@ const Post = (props) => {
 
   const getData = async () => {
     const { data } = await axios.get(`/api/posts/${props.match.params.id}`)
+    console.log(data)
     setPostData(data)
+  }
+
+  const comment = async (e) => {
+    e.preventDefault()
+    const { data } = await axios.post(`/api/posts/${postData._id}`, { text: commentData }, {
+      headers: {
+        Authorization: `Bearer ${Auth.getToken()}`
+      }
+    })
+    getData()
+  }
+
+  const onChangeComment = (e) => {
+    setCommentData(e.target.value)
   }
 
   return (
     <div>
+      <Link to={`/boards/${postData.board}`}><div className="board_banner"></div></Link>
       <h1 className="post_title">{postData.title}</h1>
+      <form onSubmit={comment}>
+        <label htmlFor="comment">Comment</label>
+        <textarea 
+          name="comment"
+          onChange={onChangeComment}
+          value={commentData}
+        />
+        <div>
+          <input type="submit"/>
+        </div>
+      </form>
       {postData.comments && postData.comments.map(comment => (
         <div key={comment._id}>{comment.text}</div>
       ))}
